a24bdba60b506e2ecaf9e197f96bc6490f145c77
Christopher Wells <windssoulx7@gmail.com>
2021-01-22 12:39:52 -0800

    Bootstrapped project with Spring Boot

---
+++ src/main/java/com/alasia/taskdo/TaskdoApplication.java
@@ -0,0 +1,13 @@
+package com.alasia.taskdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskdoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alasia/taskdo/TaskdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alasia.taskdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
